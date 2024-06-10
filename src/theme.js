e00075f95da7734841ee22248a2737d5a70ecef1
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-06-10 21:27:41 +0200

    basic config (files, styled comp, themes, Global style, Normalize, Fonts)

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        textSecondary: "#6E7E91",
+        mainBlueLM: "#0366D6",
+        mainBlueDM: "#2188FFh",
+        bgLightMode: "#FBFBFEh",
+        semiDark: "#363636B8",
+        blackText: "#252525",
+    },
+    breakpoint: {
+        medium: 767,
+        small: 480,
+    }
+};
