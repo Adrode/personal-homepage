c8e71e3d5ed3a2ed81a4889e46ec44e7664d9ac8
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-07-13 22:16:20 +0200

    font-weight themes

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,20 @@
 export const theme = {
-    color: {
-        white: "#FFFFFF",
-        textSecondary: "#6E7E91",
-        mainBlueLM: "#0366D6",
-        mainBlueDM: "#2188FF",
-        bgLightMode: "#FBFBFE",
-        semiDark: "#363636B8",
-        blackText: "#252525",
-    },
-    breakpoint: {
-        medium: 767,
-        small: 480,
-    }
+  color: {
+    white: "#FFFFFF",
+    borderTransparent: "#D1D5DA4D",
+    textSecondary: "#6E7E91",
+    mainBlueLM: "#0366D6",
+    mainBlueDM: "#2188FF",
+    bgLightMode: "#FBFBFE",
+    semiDark: "#363636B8",
+    blackText: "#252525",
+  },
+  breakpoint: {
+    medium: 767,
+    small: 480,
+  },
+  fontWeight: {
+    bold: 700,
+    heavy: 900,
+  }
 };
