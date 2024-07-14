c0f2698b91630f3b76f17dffa2804303fe7df126
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-07-14 22:04:59 +0200

    media queries for Heading

--- src/Heading/styled.js
+++ src/Heading/styled.js
@@ -1,15 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import profileImage from "../images/profile-image.jpg";
 
 export const Header = styled.header`
   display: grid;
   grid-template-columns: auto 1fr;
-  grid-gap: 72px;
+  grid-template-rows: auto 1fr;
+  grid-template-areas:
+    "picture switch"
+    "picture description"
+  ;
+  grid-column-gap: 72px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    grid-template-areas:
+      "picture switch"
+      "description description"
+    ;
+    grid-column-gap: 0px;
+    grid-row-gap: 16px;
+  }
 `;
 
-export const InformationContainer = styled.div``;
-
 export const Image = styled.img`
+  grid-area: picture;
   background-image: url(${profileImage});
   background-repeat: no-repeat;
   background-position: center;
@@ -17,14 +30,33 @@ export const Image = styled.img`
   width: 384px;
   height: 384px;
   border-radius: 50%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    width: 285px;
+    height: 285px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    width: 128px;
+    height: 128px;
+  }
 `;
 
 export const SwitchContainer = styled.div`
+  grid-area: switch;
   display: grid;
   grid-template-columns: auto auto;
   justify-content: right;
   align-items: center;
   grid-gap: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    align-self: baseline;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    grid-template-columns: none;
+  }
 `;
 
 export const SwitchDescription = styled.span`
@@ -33,44 +65,73 @@ export const SwitchDescription = styled.span`
   white-space: nowrap;
   font-size: 12px;
   text-transform: uppercase;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    display: none;
+  }
 `;
 
 export const SwitchToggle = styled.img`
   height: 26px;
 `;
 
-export const Description = styled.section``;
+export const Description = styled.section`
+  grid-area: description;
+`;
 
 export const Subheader = styled.p`
   color: ${({ theme }) => theme.color.textSecondary};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   text-transform: uppercase;
   font-size: 12px;
-  margin-bottom: 12px;
+  margin: 0 0 12px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    margin-bottom: 8px; 
+  }
 `;
 
 export const Name = styled.h1`
   color: ${({ theme }) => theme.color.blackText};
   font-weight: ${({ theme }) => theme.fontWeight.heavy};
   font-size: 38px;
-  margin-bottom: 35px;
+  margin: 0 0 35px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-size: 22px;
+    margin-bottom: 16px;
+  }
 `;
 
 export const PersonalDescription = styled.p`
   color: ${({ theme }) => theme.color.textSecondary};
   font-size: 20px;
   line-height: 28px;
-  margin-bottom: 32px;
+  margin: 0 0 32px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-size: 17px;
+    margin-bottom: 24px;
+    line-height: 23.8px;
+  }
 `;
 
 export const Button = styled.button`
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  grid-gap: 16px;
+
+  font-size: 20px;
   color: ${({ theme }) => theme.color.white};
   background-color: ${({ theme }) => theme.color.mainBlueLM};
+  text-transform: capitalize;
+
   padding: 12px 16px;
   border: 1px solid ${({ theme }) => theme.color.borderTransparent};
   border-radius: 4px;
 
-  &:focus, &:hover {
+  &:hover {
     box-shadow: -2px -2px 0px 0px #8CC2FF,
       2px 2px 0px 0px #8CC2FF,
       -2px 2px 0px 0px #8CC2FF,
@@ -80,4 +141,11 @@ export const Button = styled.button`
   &:active {
     box-shadow: 0px 2px 0px 0px #14462033 inset;
   }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    grid-gap: 12px;
+    font-size: 18px;
+  }
 `;
+
+export const ButtonIcon = styled.img``;
--- src/Wrapper/styled.js
+++ src/Wrapper/styled.js
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.main`
     max-width: 1216px;
-    margin-top: 119px;
-    border: 1px solid black;
+    margin: 119px 16px 0 16px;
 
     display: flex;
     flex-direction: column;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+        margin-top: 21px;
+    }
 `;
